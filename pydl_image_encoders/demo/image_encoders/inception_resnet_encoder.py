4684bff0d084645681ba545ee3ab0594be4f9001
xschen <xs0040@gmail.com>
2018-02-09 10:45:30 +0800

    demo script for inception resnet

---
+++ pydl_image_encoders/demo/image_encoders/inception_resnet_encoder.py
@@ -0,0 +1,18 @@
+from pydl_image_encoders.library.inception_resnet import InceptionResNetImageEnoder
+
+
+def main():
+    data_dir_path = '../very_large_data'
+    img_dir_path = '../data/images'
+    sample_images = [img_dir_path + '/dog.jpg', img_dir_path + '/cat.jpg']
+    encoder = InceptionResNetImageEnoder()
+    encoder.load_model(data_dir_path)
+    for image_path in sample_images:
+        label, class_id, predict_score = encoder.predict_image_file(image_path)
+        print(encoder.encode_image_file(image_path, True).shape)
+        print(encoder.encode_image_file(image_path, False).shape)
+        print(label, class_id, predict_score)
+
+
+if __name__ == '__main__':
+    main()
