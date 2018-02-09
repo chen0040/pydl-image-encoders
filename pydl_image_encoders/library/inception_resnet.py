b05e09005a83abe1dd6f2dfed05c97e71d22ca33
xschen <xs0040@gmail.com>
2018-02-09 10:34:22 +0800

    refactor the inception_resnet

--- pydl_image_encoders/library/inception_resnet.py
+++ pydl_image_encoders/library/inception_resnet.py
@@ -35,28 +35,60 @@ def download_pretrained_model(data_dir_path):
     tar.close()
 
 
-download_pretrained_model('../demo/very_large_data')
+class InceptionResNetImageEnoder(object):
 
-checkpoint_file = '../demo/vary_large_data/' + INCEPTION_RESNET_MODEL_NAME
+    def __init__(self):
+        self.sess = None
+        input_tensor = tf.placeholder(tf.float32, shape=(None, 299, 299, 3), name='input_image')
+        self.input_tensor = input_tensor
+        scaled_input_tensor = tf.scalar_mul((1.0 / 255), input_tensor)
+        scaled_input_tensor = tf.subtract(scaled_input_tensor, 0.5)
+        scaled_input_tensor = tf.multiply(scaled_input_tensor, 2.0)
+        self.scaled_input_tensor = scaled_input_tensor
+        self.sess = tf.Session()
+        arg_scope = inception_resnet_v2_arg_scope()
+        with slim.arg_scope(arg_scope):
+            self.logits, self.end_points = inception_resnet_v2(self.scaled_input_tensor, is_training=False)
+        self.saver = tf.train.Saver()
 
-sample_images = ['../demo/data/images/dog.jpg', '../demo/data/images/cat.jpg']
+    def load_model(self, data_dir_path):
+        download_pretrained_model(data_dir_path)
+        checkpoint_file = os.path.join(data_dir_path, INCEPTION_RESNET_MODEL_NAME)
+        self.saver.restore(self.sess, checkpoint_file)
 
-input_tensor = tf.placeholder(tf.float32, shape=(None,299,299,3), name='input_image')
-scaled_input_tensor = tf.scalar_mul((1.0/255), input_tensor)
-scaled_input_tensor = tf.subtract(scaled_input_tensor, 0.5)
-scaled_input_tensor = tf.multiply(scaled_input_tensor, 2.0)
+    def predict_image(self, im):
+        im = im.reshape(-1, 299, 299, 3)
+        predict_values, logit_values = self.sess.run([self.end_points['Predictions'], self.logits],
+                                                     feed_dict={self.input_tensor: im})
+        return np.argmax(predict_values), np.max(predict_values)  # , np.argmax(logit_values)
 
-#Load the model
-sess = tf.Session()
-arg_scope = inception_resnet_v2_arg_scope()
-with slim.arg_scope(arg_scope):
-  logits, end_points = inception_resnet_v2(scaled_input_tensor, is_training=False)
-saver = tf.train.Saver()
-saver.restore(sess, checkpoint_file)
-for image in sample_images:
-  im = Image.open(image).resize((299,299))
-  im = np.array(im)
-  im = im.reshape(-1,299,299,3)
-  predict_values, logit_values = sess.run([end_points['Predictions'], logits], feed_dict={input_tensor: im})
-  print (np.max(predict_values), np.max(logit_values))
-  print (np.argmax(predict_values), np.argmax(logit_values))
+    def predict_image_file(self, image_path):
+        im = Image.open(image_path).resize((299, 299))
+        im = np.array(im)
+        return self.predict_image(im)
+
+    def encode_image(self, im):
+        im = im.reshape(-1, 299, 299, 3)
+        predict_values, logit_values = self.sess.run([self.end_points['Predictions'], self.logits],
+                                                     feed_dict={self.input_tensor: im})
+        return predict_values
+
+    def encode_image_file(self, image_path):
+        im = Image.open(image_path).resize((299, 299))
+        im = np.array(im)
+        return self.encode_image(im)
+
+
+def main():
+    data_dir_path = '../demo/very_large_data'
+    img_dir_path = '../demo/data/images'
+    sample_images = [img_dir_path + '/dog.jpg', img_dir_path + '/cat.jpg']
+    encoder = InceptionResNetImageEnoder()
+    encoder.load_model(data_dir_path)
+    for image_path in sample_images:
+        class_id, predict_score = encoder.predict_image_file(image_path)
+        print(class_id, predict_score)
+
+
+if __name__ == '__main__':
+    main()
